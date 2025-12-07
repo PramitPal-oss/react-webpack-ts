ca63c2170e3c66ad0dc50114b262503debc6d023
Pramit Pal <pramitpal309@gmail.com>
2025-12-07 12:06:31 +0530

    feat: add product fetching functionality and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import classes from './app.module.css';
 import webLogo from './assets/logo/webpackLogoo.png';
 
 export default function App() {
   const [input, setInput] = useState<string>('');
+  const [products, setProducts] = useState<Array<any>>([]);
   console.log(process.env.USER_NAME, 'ENV File');
+
+  const fetchAllProducts = useCallback(async () => {
+    try {
+      const response: Response = await fetch('https://dummyjson.com/products');
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, [fetchAllProducts]);
+
   return (
     <div>
       <h1 className={classes.heading}>Hello from React + Webpack</h1>
