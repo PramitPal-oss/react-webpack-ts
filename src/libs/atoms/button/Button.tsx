6f167a96b22c7d27fafa5fe92244ddeb462b353d
Pramit Pal <pramitpal309@gmail.com>
2025-12-16 00:29:38 +0530

    feat: make RCButton props optional for variant, size, and radius

--- src/libs/atoms/button/Button.tsx
+++ src/libs/atoms/button/Button.tsx
@@ -4,16 +4,16 @@ import * as React from 'react';
 
 interface RCButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  variant: 'filled' | 'outline' | 'light' | 'subtle' | 'default' | 'white' | 'gradient';
-  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-  radius: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | number;
+  variant?: 'filled' | 'outline' | 'light' | 'subtle' | 'default' | 'white' | 'gradient';
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  radius?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | number;
   className?: string;
   leftSection?: React.ReactNode;
   rightSection?: React.ReactNode;
   fullWidth?: boolean;
   disabled?: boolean;
   loading?: boolean;
-  color:
+  color?:
     | 'dark'
     | 'gray'
     | 'red'
@@ -35,7 +35,14 @@ const theme = createTheme({
   fontFamily: 'Poppins, sans-serif',
 });
 
-const RCButton: React.FC<RCButtonProps> = ({ variant = 'filled', color = 'blue', children, ...rest }) => {
+const RCButton: React.FC<RCButtonProps> = ({
+  variant = 'filled',
+  color = 'blue',
+  size = 'md',
+  radius = 'md',
+  children,
+  ...rest
+}) => {
   return (
     <MantineProvider theme={theme}>
       <Button variant={variant} color={color} {...rest}>
